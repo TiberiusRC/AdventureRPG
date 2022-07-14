49ec4e62ef8cf99c034d490c16aec428dccaf163
Ronald Cober <Ronald.cober.bee@outlook.com>
2022-07-15 00:24:40 +0200

    Added more classes to the Engine

---
+++ Engine/HealingPotion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Engine
+{
+    public class HealingPotion
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int AmountToHeal { get; set; }
+    }
+}
