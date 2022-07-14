7ca74f324378e6d47140b0f892aff7664aa70c60
Ronald Cober <Ronald.cober.bee@outlook.com>
2022-07-15 00:17:09 +0200

    Removed Test btn , added playerclass to engine and initialized player in main form

--- AdventureRPG/AdventureRPG.cs
+++ AdventureRPG/AdventureRPG.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,23 @@ namespace AdventureRPG
 {
     public partial class AdventureRPG : Form
     {
+        private Player _player;
         public AdventureRPG()
         {
             InitializeComponent();
+            _player = new Player();
+            //Player properties
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+            //Player properties to label
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
-
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            lblGold.Text = "123";
-        }
+                
     }
 }
