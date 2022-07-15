8b66af7030691e8a2719d60180fd46661a666f9f
Ronald Cober <Ronald.cober.bee@outlook.com>
2022-07-15 09:33:16 +0200

    Added first Ctor's

--- AdventureRPG/AdventureRPG.cs
+++ AdventureRPG/AdventureRPG.cs
@@ -17,6 +17,12 @@ namespace AdventureRPG
         public AdventureRPG()
         {
             InitializeComponent();
+
+            Location location = new Location(1, "Home", "This is your house.");
+            location.ID = 1;
+            location.Name = "Home";
+            location.Description = "This is your house.";
+
             _player = new Player();
             //Player properties
             _player.CurrentHitPoints = 10;
